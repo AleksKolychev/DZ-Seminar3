3726ef552c14bfb288716c299ef50fdb5a28fb04
AleksKolychev <aleksushome@gmail.com>
2023-02-07 18:58:12 +0500

    Add Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (((n - n % 10000) / 10000 == n % 10) && (((n - n % 1000) /1000) % 10 == (n % 100 - n % 10) / 10))
+Console.WriteLine("yes");
+else 
+Console.WriteLine("no");
